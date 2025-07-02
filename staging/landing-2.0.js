349f259317f75eecbb744d7b8a77da837f150077
Kalle Westerling <kalle.westerling@gmail.com>
2025-07-02 12:09:09 +0100

    Adding debug message

--- staging/landing-2.0.js
+++ staging/landing-2.0.js
@@ -1,1 +1,5 @@
 // This file is for our new landing page design, for testing purposes currently.
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.info("Landing 2.0 script loaded");
+})
