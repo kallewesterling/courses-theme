2f143310eecccc0e30b60052f36fb739273b7eb1
Kalle Westerling <kalle.westerling@gmail.com>
2025-07-03 13:58:00 +0100

    Testing hooking into landing page

--- staging/landing-2.0.js
+++ staging/landing-2.0.js
@@ -1,5 +1,12 @@
 // This file is for our new landing page design, for testing purposes currently.
 
+const elems = {
+    collections: {}
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     console.info("Landing 2.0 script loaded");
-})
+    
+    elems.header = document.querySelector(".page-content-block.html-content-block[data-index='0']");
+    elems.collections.h2 = $('h2');
+});
