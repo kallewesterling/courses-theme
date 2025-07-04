0d1106cab19294eee88d7dd9083739dafc7c293d
Kalle Westerling <kalle.westerling@gmail.com>
2025-07-04 17:11:01 +0100

    Moving logic

--- staging/shiki.js
+++ staging/shiki.js
@@ -27,23 +27,16 @@ function addCopyButton(options = {}) {
     }
 }
 
+document.querySelectorAll("code[data-lang]").forEach(async (codeElement) => {
+    const lang = codeElement.dataset.lang;
+    const code = codeElement.textContent.trim();
+    
+    const html = await codeToHtml(code, { lang, theme: THEME, transformers: [addCopyButton({toggle: 2000})] });
+    
+    const parent = codeElement.parentElement;
+    parent.outerHTML = html; // Replace the code element with highlighted HTML
+    parent.dataset.lang = lang; // Preserve the language in the parent element
+});
 
 document.addEventListener("DOMContentLoaded", async () => {
-    console.log("DOM fully loaded and parsed");
-
-    document.querySelectorAll("code[data-lang]").forEach(async (codeElement) => {
-        const lang = codeElement.dataset.lang;
-        const code = codeElement.textContent.trim();
-        
-        const html = await codeToHtml(code, { lang, theme: THEME, transformers: [addCopyButton({toggle: 2000})] });
-        
-        const parent = codeElement.parentElement;
-        parent.outerHTML = html; // Replace the code element with highlighted HTML
-        parent.dataset.lang = lang; // Preserve the language in the parent element
-        
-        console.log("--------");
-        console.log("INFO");
-        console.log(parent, parent.dataset);
-        console.log("--------");
-    });
 });
