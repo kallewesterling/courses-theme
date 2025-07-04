31f716e248514a0f0d8a202f8bf1a05ab548185c
Kalle Westerling <kalle.westerling@gmail.com>
2025-07-04 16:15:03 +0100

    Adding copy button

--- staging/shiki.js
+++ staging/shiki.js
@@ -4,6 +4,29 @@ const THEME = 'min-light'; // Shiki theme to use
 
 import { createHighlighter } from 'https://esm.sh/shiki@3.0.0'
 
+function addCopyButton(options = {}) {
+  const toggleMs = options.toggle || 3000
+
+  return {
+    name: 'shiki-transformer-copy-button',
+    pre(node) {
+      const button = h(
+        'button',
+        {
+          class: 'copy',
+          onclick: `
+          navigator.clipboard.writeText(this.parentElement.innerText);
+          this.classList.add('copied');
+          setTimeout(() => this.classList.remove('copied'), ${toggleMs})
+        `
+        },
+        [h('span', { class: 'ready' }), h('span', { class: 'success' })]
+      )
+
+      node.children.push(button)
+    }
+  }
+}
 
 // await document loaded
 document.addEventListener("DOMContentLoaded", async () => {
@@ -11,13 +34,13 @@ document.addEventListener("DOMContentLoaded", async () => {
     
     const langs = document.querySelectorAll("code[data-lang]").forEach(codeElement => codeElement.dataset.lang) || [];
 
-    window.highlighter = await createHighlighter({themes: [THEME], langs})
+    highlighter = await createHighlighter({themes: [THEME], langs})
 
     document.querySelectorAll("code[data-lang]").forEach(async (codeElement) => {
         const lang = codeElement.dataset.lang;
         const code = codeElement.textContent.trim();
         
-        const html = await window.highlighter.codeToHtml(code, { lang, theme: THEME });
+        const html = await highlighter.codeToHtml(code, { lang, theme: THEME, transformers: [addCopyButton({toggle: 2000})] });
         
         const parent = codeElement.parentElement;
         parent.outerHTML = html; // Replace the code element with highlighted HTML
