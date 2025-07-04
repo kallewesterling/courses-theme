eb5197ae2006c254c7078d2945c948ff227b9502
Kalle Westerling <kalle.westerling@gmail.com>
2025-07-04 15:09:50 +0100

    Testing shiki

--- staging/shiki.js
+++ staging/shiki.js
@@ -1,8 +1,12 @@
 // indicate that it is a module
 
+const THEME = 'vitesse-dark'; // Shiki theme to use
+
 import { codeToHtml } from 'https://esm.sh/shiki@3.0.0'
 
-const code = 'const a = 1' // input code
-const html = await codeToHtml(code, {lang: 'javascript', theme : 'vitesse-dark'})
-
-console.log(html) // highlighted html string
+document.querySelectorAll("code[data-lang]").forEach(async (codeElement) => {
+    const lang = codeElement.dataset.lang;
+    const code = codeElement.textContent.trim();
+    const html = await codeToHtml(code, { lang, theme: THEME });
+    codeElement.parentElement.outerHTML = html; // Replace the code element with highlighted HTML
+}
