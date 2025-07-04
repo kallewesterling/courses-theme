198b8f5200598988e2c40adac5c15104875fdf0e
Kalle Westerling <kalle.westerling@gmail.com>
2025-07-04 16:05:26 +0100

    Swirching to `createHighlighter`

--- staging/shiki.js
+++ staging/shiki.js
@@ -2,18 +2,22 @@
 
 const THEME = 'min-light'; // Shiki theme to use
 
-import { codeToHtml } from 'https://esm.sh/shiki@3.0.0'
+import { createHighlighter } from 'https://esm.sh/shiki@3.0.0'
 
 
 // await document loaded
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     console.log("DOM fully loaded and parsed");
+    
+    const langs = document.querySelectorAll("code[data-lang]").forEach(codeElement => codeElement.dataset.lang) || [];
+
+    window.highlighter = await createHighlighter({themes: [THEME], langs})
 
     document.querySelectorAll("code[data-lang]").forEach(async (codeElement) => {
         const lang = codeElement.dataset.lang;
         const code = codeElement.textContent.trim();
         
-        const html = await codeToHtml(code, { lang, theme: THEME });
+        const html = await window.highlighter.codeToHtml(code, { lang, theme: THEME });
         
         const parent = codeElement.parentElement;
         parent.outerHTML = html; // Replace the code element with highlighted HTML
@@ -22,13 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("--------");
         console.log("INFO");
         console.log(parent, parent.dataset);
-        console.log("--------");
-    });
-
-    document.querySelectorAll("code[data-lang]").forEach((codeElement) => {
-        console.log("Elem before", codeElement.parentElement.style.backgroundColor);
-        codeElement.parentElement.style.backgroundColor = `${codeElement.parentElement.style.backgroundColor} !important`;
-        console.log("Elem after", codeElement.parentElement.style.backgroundColor);
         console.log("--------");
     });
 });
