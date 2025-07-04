5cb74e75a6eabe24d6dc871b161697125d10946f
Kalle Westerling <kalle.westerling@gmail.com>
2025-07-04 15:19:06 +0100

    Fixing bg color?

--- staging/shiki.js
+++ staging/shiki.js
@@ -1,6 +1,6 @@
 // indicate that it is a module
 
-const THEME = 'vitesse-dark'; // Shiki theme to use
+const THEME = 'min-light'; // Shiki theme to use
 
 import { codeToHtml } from 'https://esm.sh/shiki@3.0.0'
 
@@ -9,6 +9,7 @@ document.querySelectorAll("code[data-lang]").forEach(async (codeElement) => {
     const code = codeElement.textContent.trim();
     const html = await codeToHtml(code, { lang, theme: THEME });
     codeElement.parentElement.outerHTML = html; // Replace the code element with highlighted HTML
+    codeElement.parentElement.style.backgroundColor = `${codeElement.parentElement.style.backgroundColor} !important`;
     console.log(codeElement);
     console.log(html);
     console.log("--------");
