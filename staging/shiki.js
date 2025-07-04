1dfbf9c6bf6316e2ecec1971ea2ed1113fafbf18
Kalle Westerling <kalle.westerling@gmail.com>
2025-07-04 15:55:49 +0100

    Testing it here

--- staging/shiki.js
+++ staging/shiki.js
@@ -4,20 +4,26 @@ const THEME = 'min-light'; // Shiki theme to use
 
 import { codeToHtml } from 'https://esm.sh/shiki@3.0.0'
 
-document.querySelectorAll("code[data-lang]").forEach(async (codeElement) => {
-    const lang = codeElement.dataset.lang;
-    const code = codeElement.textContent.trim();
-    const html = await codeToHtml(code, { lang, theme: THEME });
-    codeElement.parentElement.outerHTML = html; // Replace the code element with highlighted HTML
-    codeElement.parentElement.dataset.lang = lang; // Preserve the language in the parent element
-    console.log(codeElement);
-    console.log(html);
-    console.log("--------");
-});
 
 // await document loaded
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM fully loaded and parsed");
+
+    document.querySelectorAll("code[data-lang]").forEach(async (codeElement) => {
+        const lang = codeElement.dataset.lang;
+        const code = codeElement.textContent.trim();
+        
+        const html = await codeToHtml(code, { lang, theme: THEME });
+        
+        const parent = codeElement.parentElement;
+        parent.outerHTML = html; // Replace the code element with highlighted HTML
+        parent.dataset.lang = lang; // Preserve the language in the parent element
+        
+        console.log("--------");
+        console.log("INFO");
+        console.log(parent, parent.dataset);
+        console.log("--------");
+    });
 
     document.querySelectorAll("code[data-lang]").forEach((codeElement) => {
         console.log("Elem before", codeElement.parentElement.style.backgroundColor);
