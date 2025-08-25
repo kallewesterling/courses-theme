a225d52cb177c20a048d70ff9cf83cee227d3d51
Kalle Westerling <kalle.westerling@gmail.com>
2025-08-25 17:29:24 +0100

    Wrong order

--- production/skilljar-theme-v2.1.js
+++ production/skilljar-theme-v2.1.js
@@ -930,8 +930,12 @@ function styleCurriculumPageNoCertificate() {
     },
     tabs: {
       container: document.querySelector(".tabs"),
-      curriculumSection: document.querySelector(".tabs section #curriculumSection") || document.querySelector(".tabs section:nth-child(1)"),
-      aboutSection: document.querySelector(".tabs section #aboutSection") || document.querySelector(".tabs section:nth-child(2)"),
+      curriculumSection:
+        document.querySelector(".tabs section #curriculumSection") ||
+        document.querySelector(".tabs section:nth-child(1)"),
+      aboutSection:
+        document.querySelector(".tabs section #aboutSection") ||
+        document.querySelector(".tabs section:nth-child(2)"),
     },
     card: {
       details: document.querySelector(".course-details-card"),
@@ -956,7 +960,7 @@ function styleCurriculumPageNoCertificate() {
   if (!initialLoadComplete) {
     v.local.tabs.aboutSection.id = "aboutSection";
     v.local.tabs.curriculumSection.id = "curriculumSection";
-    
+
     v.local.card.detailItems.forEach((li) =>
       li.prepend(createClone("checkbox"))
     );
@@ -982,8 +986,8 @@ function styleCurriculumPageNoCertificate() {
     ].filter(Boolean)
   );
   v.local.tabs.container.append(
-    v.local.tabs.curriculumSection,
-    v.local.tabs.aboutSection
+    v.local.tabs.aboutSection,
+    v.local.tabs.curriculumSection
   );
 }
 
