b23a2fbf63fae15545b751a9e7a6c92574730b05
Kalle Westerling <kalle.westerling@gmail.com>
2025-09-12 17:37:00 +0100

    Fixing ReferenceError

--- production/skilljar-theme-v2.1.js
+++ production/skilljar-theme-v2.1.js
@@ -795,7 +795,7 @@ function styleLanding() {
 
   v.local.catalogBodyParentContainer.append(v.local.catalogContainer);
 
-  hide(mainContentContainer);
+  hide(v.local.mainContentContainer);
 }
 
 /**
