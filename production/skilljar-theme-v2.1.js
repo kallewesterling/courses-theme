a3a4abfea427034d295db63e44db4ff480fd0463
Kalle Westerling <kalle.westerling@gmail.com>
2025-12-29 17:34:53 +0100

    Removing auto width and height on createClone

--- production/skilljar-theme-v2.1.js
+++ production/skilljar-theme-v2.1.js
@@ -1196,8 +1196,8 @@ function createClone(
   type = "checkbox",
   attrs = {
     xmlns: "http://www.w3.org/2000/svg",
-    width: "20",
-    height: "21",
+    // width: "20",
+    // height: "21",
     viewBox: "0 0 20 21",
     // fill: "none",
   }
