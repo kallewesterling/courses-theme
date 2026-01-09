4f00ee8e9efc8658ead1d57991830b722d831fe8
Kalle Westerling <kalle.westerling@gmail.com>
2026-01-09 17:03:16 +0100

    Adding in a `DOMContentLoaded` listener

--- production/theme.mjs
+++ production/theme.mjs
@@ -4,13 +4,15 @@ import { footerData } from "./skilljar-theme-v3.0/static.mjs";
 import { animateCompletion } from "./skilljar-theme-v3.0/course-completion.mjs";
 import { generateFooter } from "./skilljar-theme-v3.0/footer.mjs";
 
-window.CG = CG;
-window.Q = Q;
-window.A = A;
-window.c = c;
-window.getCorrectURL = getCorrectURL;
-window.el = el;
-window.footerData = footerData;
+document.addEventListener("DOMContentLoaded", () => {
+  window.CG = CG;
+  window.Q = Q;
+  window.A = A;
+  window.c = c;
+  window.getCorrectURL = getCorrectURL;
+  window.el = el;
+  window.footerData = footerData;
 
-generateFooter(window.footerData);
-window.animateCompletion = animateCompletion;
+  generateFooter(window.footerData);
+  window.animateCompletion = animateCompletion;
+});
