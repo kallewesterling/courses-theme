8d8d245dcd09690332e55c18e699557d8bffdc7c
Kalle Westerling <kalle.westerling@gmail.com>
2026-01-09 17:31:02 +0100

    Exposing `hide` and `showBody`

--- production/theme.mjs
+++ production/theme.mjs
@@ -10,6 +10,7 @@ import { CG } from "./skilljar-theme-v3.0/CG.mjs";
 import { CONFIG, footerData } from "./skilljar-theme-v3.0/static.mjs";
 import { animateCompletion } from "./skilljar-theme-v3.0/course-completion.mjs";
 import { generateFooter } from "./skilljar-theme-v3.0/footer.mjs";
+import { hide, showBody } from "./skilljar-theme-v3.0/styling.mjs";
 import { logger } from "./skilljar-theme-v3.0/logger.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -21,6 +22,8 @@ document.addEventListener("DOMContentLoaded", () => {
   window.getCorrectURL = getCorrectURL;
   window.el = el;
   window.remove = remove;
+  window.hide = hide;
+  window.showBody = showBody;
   window.logger = logger;
   window.footerData = footerData;
 
