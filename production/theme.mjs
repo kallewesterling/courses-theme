a1c32ff5d3da1c2ff3ce84496465a9fd762f2702
Kalle Westerling <kalle.westerling@gmail.com>
2026-01-09 16:30:36 +0100

    Can I expose the functions like this?

--- production/theme.mjs
+++ production/theme.mjs
@@ -1,3 +1,9 @@
 import { Q, A, c, getCorrectURL, el } from "./skilljar-theme-v3.0/meta.mjs";
 import { footerData } from "./skilljar-theme-v3.0/static.mjs";
 
+window.Q = Q;
+window.A = A;
+window.c = c;
+window.getCorrectURL = getCorrectURL;
+window.el = el;
+window.footerData = footerData;
