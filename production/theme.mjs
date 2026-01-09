b73447d5291d838911015213977cc463849d9d03
Kalle Westerling <kalle.westerling@gmail.com>
2026-01-09 17:20:56 +0100

    Exposing `CONFIG` object

--- production/theme.mjs
+++ production/theme.mjs
@@ -7,12 +7,13 @@ import {
   remove,
 } from "./skilljar-theme-v3.0/meta.mjs";
 import { CG } from "./skilljar-theme-v3.0/CG.mjs";
-import { footerData } from "./skilljar-theme-v3.0/static.mjs";
+import { CONFIG, footerData } from "./skilljar-theme-v3.0/static.mjs";
 import { animateCompletion } from "./skilljar-theme-v3.0/course-completion.mjs";
 import { generateFooter } from "./skilljar-theme-v3.0/footer.mjs";
 import { logger } from "./skilljar-theme-v3.0/logger.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
+  window.CONFIG = CONFIG;
   window.CG = CG;
   window.Q = Q;
   window.A = A;
