8cbd96bb3b6979bef1531c2d6c905d75e12bc78b
Kalle Westerling <kalle.westerling@gmail.com>
2026-01-09 17:18:26 +0100

    Exposing `logger` function

--- production/theme.mjs
+++ production/theme.mjs
@@ -10,6 +10,7 @@ import { CG } from "./skilljar-theme-v3.0/CG.mjs";
 import { footerData } from "./skilljar-theme-v3.0/static.mjs";
 import { animateCompletion } from "./skilljar-theme-v3.0/course-completion.mjs";
 import { generateFooter } from "./skilljar-theme-v3.0/footer.mjs";
+import { logger } from "./skilljar-theme-v3.0/logger.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   window.CG = CG;
@@ -19,6 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
   window.getCorrectURL = getCorrectURL;
   window.el = el;
   window.remove = remove;
+  window.logger = logger;
   window.footerData = footerData;
 
   generateFooter(window.footerData);
