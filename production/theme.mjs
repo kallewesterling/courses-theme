464c6b9ad20d1ebdf47071a657f2c46500457423
Kalle Westerling <kalle.westerling@gmail.com>
2026-01-09 17:37:30 +0100

    Exposing `pathSections`

--- production/theme.mjs
+++ production/theme.mjs
@@ -7,7 +7,11 @@ import {
   remove,
 } from "./skilljar-theme-v3.0/meta.mjs";
 import { CG } from "./skilljar-theme-v3.0/CG.mjs";
-import { CONFIG, footerData } from "./skilljar-theme-v3.0/static.mjs";
+import {
+  CONFIG,
+  footerData,
+  pathSections,
+} from "./skilljar-theme-v3.0/static.mjs";
 import { animateCompletion } from "./skilljar-theme-v3.0/course-completion.mjs";
 import { generateFooter } from "./skilljar-theme-v3.0/footer.mjs";
 import { hide, showBody } from "./skilljar-theme-v3.0/styling.mjs";
@@ -15,6 +19,8 @@ import { logger } from "./skilljar-theme-v3.0/logger.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   window.CONFIG = CONFIG;
+  window.footerData = footerData;
+  window.pathSections = pathSections;
   window.CG = CG;
   window.Q = Q;
   window.A = A;
@@ -25,7 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
   window.hide = hide;
   window.showBody = showBody;
   window.logger = logger;
-  window.footerData = footerData;
 
   generateFooter(window.footerData);
   window.animateCompletion = animateCompletion;
