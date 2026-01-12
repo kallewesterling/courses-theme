2f1009c56f174d8d40a99f606b4a0745b16344ad
Kalle Westerling <kalle.westerling@gmail.com>
2026-01-12 15:44:34 +0100

    Changing the logic of the script

--- production/preload.js
+++ production/preload.js
@@ -1,1 +1,7 @@
+/*
+ * Chainguard Courses Theme v4.0
+ * This preload script is executed before the main theme script.
+ * It sets the body's display to "none" to prevent FOUC (Flash of Unstyled Content).
+ * See the main theme script for further details (theme.mjs).
+*/
 document.querySelector("body").style.setProperty("display", "none");
