11e447373bd0669dd5315cb846f3e59cea3497fa
Kalle Westerling <kalle.westerling@gmail.com>
2026-01-13 16:36:41 +0100

    Cleaning up unused files

--- staging/landing-2.0.js
+++
@@ -1,5 +0,0 @@
-// This file is for our new landing page design, for testing purposes currently.
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.info("Landing 2.0 script loaded");
-})
